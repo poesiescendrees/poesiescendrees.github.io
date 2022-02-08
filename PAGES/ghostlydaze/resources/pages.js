6db6308f3fde1b15a5123ad3d4a5f7dbc88d3b2f
emsyz <lanne.berig@gmail.com>
2022-02-08 12:00:27 +0100

    test templates ghostly daze

---
+++ PAGES/ghostlydaze/resources/pages.js
@@ -0,0 +1,33 @@
+$(function() {
+  var nw = $('#NEWESTUSER a').eq(0);
+  var toput = $('#AVATARUSER');
+  // $(function(){
+   //  $.get($("#NEWESTUSER a[href^='/u']")[0].href,function(response){
+   //    (a=$("#MEMBER_AVATAR img",$(response))).length&&$("#AVATARUSER").html(a);}
+   //   )
+   // });
+   //
+   // $.get('./templates/general/topic_list_box.html'), function(response) {
+   //
+   // }
+
+   var xxx = gip('./templates/general/topic_list_box.html', 'body');
+   console.log(xxx);
+});
+
+
+
+/*
+    gets $(infoCSS) from the url page
+*/
+function gip(url, infoCSS) {
+    var toreturn;
+    $.ajax({
+        url : url,
+        success : function(data) {
+            toreturn = $(infoCSS, $(data))
+        },
+        async: false
+    });
+    return toreturn;
+}
