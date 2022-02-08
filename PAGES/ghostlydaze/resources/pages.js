2e5d179fcad972056fe8a3605794a3558dd46f94
emsyz <lanne.berig@gmail.com>
2022-02-08 14:58:21 +0100

    delete gd js

--- PAGES/ghostlydaze/resources/pages.js
+++ PAGES/ghostlydaze/resources/pages.js
@@ -1,18 +1,4 @@
 $(function() {
-  var nw = $('#NEWESTUSER a').eq(0);
-  var toput = $('#AVATARUSER');
-  // $(function(){
-   //  $.get($("#NEWESTUSER a[href^='/u']")[0].href,function(response){
-   //    (a=$("#MEMBER_AVATAR img",$(response))).length&&$("#AVATARUSER").html(a);}
-   //   )
-   // });
-   //
-   // $.get('./templates/general/topic_list_box.html'), function(response) {
-   //
-   // }
-
-   var xxx = gip('./templates/general/topics_list_box.html', 'body');
-   console.log(xxx);
 });
 
 
